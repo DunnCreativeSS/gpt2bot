7d72a7df6c142435200fbfa826b1ce2a5ae1afc6
Your Name <you@example.com>
2020-10-27 01:33:30 -0300

    porn and etc

--- gpt2bot/telegram_bot.py
+++ gpt2bot/telegram_bot.py
@@ -103,7 +103,7 @@ def message(self, update, context):
     turns = context.chat_data['turns']
 
     user_message = update.message.text
-    if user_message.split().length >= 128:
+    if len(user_message) >= 128:
         user_message = user_message[0:127]
     if user_message.lower() == 'bye':
         # Restart chat
